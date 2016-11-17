779522b3f9b7e7739b402ec51c7a9de8b0ce9c4d
Fcott <f.f.f.kt13@gmail.com>
2016-11-17 12:28:49 -0600

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def new
@@ -19,9 +27,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    unless current_user?(@user)
+      flash[:danger] = 'You are not authenticated'
+      redirect_to root_url
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links including logged in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, coount: 2
@@ -14,5 +18,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Help")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
+
 end
