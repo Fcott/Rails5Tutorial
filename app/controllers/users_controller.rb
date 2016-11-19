834756a06671684eed61e2ea8df2f311e43af922
Fcott <f.f.f.kt13@gmail.com>
2016-11-19 13:26:49 -0600

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def show
-
+    redirect_to root_url unless @user.activated?
   end
 
   def new
@@ -19,9 +19,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = "Signed up successfully!!"
-      redirect_to @user
+      # log_in(@user)
+      # flash[:success] = "Signed up successfully!!"
+      # redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
