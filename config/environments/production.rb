69b087ac4eb878125340dd45bbbc959a59abed3f
Fcott <f.f.f.kt13@gmail.com>
2016-11-20 11:19:46 -0600

    modified production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
   host = 'shrouded-peak-33189.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.stmp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address    =>    'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
